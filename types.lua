9451977b41d9cb1ae1b695668e274039d0422622
WriteCoin <sovtsardom@gmail.com>
2021-04-20 19:27:53 +0300

    возвращен файл types.lua с описанием объекта со строковыми именами типов

---
+++ types.lua
@@ -0,0 +1,10 @@
+Types = {
+    integer = 'integer',
+    real = 'real',
+    boolean = 'boolean',
+    string = 'string',
+    table = 'table',
+    void = 'void',
+    any = 'any',
+    ['nil'] = 'nil'
+}
